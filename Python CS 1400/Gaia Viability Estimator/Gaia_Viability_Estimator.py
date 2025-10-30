f1d571221911a2b2aa999e4c23ddaea7874b7329
Matthew Blair <matthewdblair04@gmail.com>
2025-10-29 22:26:17 -0600

    fixed everything
    added the writing to the external document.
    added validation for command line inputs
    moved the iteration tracker to the external doc writer.
    edited the equation

--- Python CS 1400/Gaia Viability Estimator/Gaia_Viability_Estimator.py
+++ Python CS 1400/Gaia Viability Estimator/Gaia_Viability_Estimator.py
@@ -1,39 +1,75 @@
-#import the command line prompts and prepares a variable and a list
+#import the command line prompts and prepares a list
 import sys
 
-current_iteration = 0
 potential_populations = []
 
+
+#prints the command line arguments so we can check them
+print(sys.argv)
+
 #uses the variables for the equation, and writes to the list
-
 def equation(initial_population, growth_rate, iterations):
-    potential_populations.append(f"{current_iteration}   {initial_population}")
-    print(potential_populations[0])
 
-    population = potential_populations[current_iteration]
+    #Adds the first value to the list so that it can work
+    potential_populations.append(initial_population)
+
+
+    #uses the reverse indexing to always have the most recent iteration
+    population = potential_populations[-1]
     for num in range(iterations):
-        population = growth_rate * population
+        population = growth_rate * population * (1- population)
+        potential_populations.append(population)
 
 
 
 
-
-
-
-
-
-#converts the command line arguments to variables, and calls the equation function
+#converts the command line inputs to variables, and calls the equation function
 def main():
-    if len(sys.argv) != 5:
-        print("Command Line Error (too many or too few arguments)")
+    #checks to make sure the command line has the proper amount of inputs
+    if len(sys.argv) > 5:
+        print("Command Line Error (too many arguments)")
         return
+    elif len(sys.argv) < 5:
+        print("Command Line Error (too few arguments)")
+        return
+    else:
+        pass
 
     initial_population = float(sys.argv[1])
     growth_rate = float(sys.argv[2])
     iterations = int(sys.argv[3])
     output = sys.argv[4]
 
+    #Validates command line inputs
+    if initial_population < 0 or initial_population > 1:
+        print("Initial Population must be between 0 and 1")
+        return
+    elif growth_rate < 0 or growth_rate > 4:
+        print("Growth rate must be between 0 and 4")
+        return
+    elif iterations < 0:
+        print("Iterations must be positive")
+        return
+    else:
+        pass
+
+
+
     equation(initial_population, growth_rate, iterations)
+    #checks that a file exicts. creates it if it doesn't and writes to it if it does
+    current_iteration = 0
+    try:
+        with open(sys.argv[4], "x") as file:
+            print("File not found, creating new file")
+            for word in potential_populations:
+                file.write(f"{current_iteration}   {word:.3f}\n")
+                current_iteration += 1
+    except FileExistsError:
+        print("File Found!")
+        with open(sys.argv[4], "w") as file:
+            for word in potential_populations:
+                file.write(f"{current_iteration}   {word:.3f}\n")
+                current_iteration += 1
 
 
 
