10a0073ecc557e6ef559344d568c00f1eecdf5f1
Matthew Blair <matthewdblair04@gmail.com>
2025-11-06 20:44:11 -0700

    Update log_analyzer.py

--- Python CS 1400/Log Analyzer/log_analyzer.py
+++ Python CS 1400/Log Analyzer/log_analyzer.py
@@ -1,41 +1,45 @@
 
-#Uses a dictionary to track the number of each message type
-log_data = {
-    "INFO": 0,
-    "WARNING": 0,
-    "ERROR": 0
-}
-
 #opens and checks the start of each line. then adds a count to the appropriate type.
-def check_log():
+def check_log(log_data):
     with open("server_logs.txt", "r") as log:
         longest_line = 0
+        longest_message = ""
 
         for line in log:
-            if line.startswith("INFO"):
+            error_messages = line.split("|")
+
+            if error_messages[0] == "INFO":
                 log_data["INFO"] += 1
-            elif line.startswith("WARNING"):
+            elif error_messages[0] == "WARNING":
                 log_data["WARNING"] += 1
-            elif line.startswith("ERROR"):
+            elif error_messages[0] == "ERROR":
                 log_data["ERROR"] += 1
             else:
                 pass
 
-
-            if len(line) > longest_line:
-                longest_line = len(line)
-        #for debugging
-        print(longest_line)
-
+            # Compares the length of the messages only(not the code) then
+            if len(error_messages[1]) > longest_line:
+                longest_line = len(error_messages[1])
+                longest_message = error_messages[1]
+        return longest_message
 
 
 def main():
-    check_log()
+    # Uses a dictionary to track the number of each message type
+    log_data = {
+        "INFO": 0,
+        "WARNING": 0,
+        "ERROR": 0
+    }
+
+
+    longest_message = check_log(log_data)
+
     print(f"""Log Summary
 INFO: {log_data["INFO"]}
 WARNING: {log_data["WARNING"]}
 ERROR: {log_data["ERROR"]}
-Longest message: 
+Longest message: {longest_message}
 """)
 
 if __name__ == "__main__":
