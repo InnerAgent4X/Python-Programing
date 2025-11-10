2bf698125563438eccc835497d195c6495c549ca
Matthew Blair <matthewdblair04@gmail.com>
2025-11-09 19:47:56 -0700

    SCOREbook

---
+++ Python CS 1400/Student Scorebook/student_scorebook.py
@@ -0,0 +1,25 @@
+import sys
+
+
+def analyze_scores(scorebook):
+
+
+
+
+
+def main():
+
+    scorebook = {
+        'mike' : 60,
+        'ben' : 89,
+        'matthew' : 100,
+        'josh' : 90
+    }
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
