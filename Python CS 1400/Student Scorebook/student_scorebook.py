59713bb6d7eee1079f990cd1b57fa422b32bb174
Matthew Blair <matthewdblair04@gmail.com>
2025-11-10 17:49:52 -0700

    finished the student scorebook

--- Python CS 1400/Student Scorebook/student_scorebook.py
+++ Python CS 1400/Student Scorebook/student_scorebook.py
@@ -1,25 +1,37 @@
 import sys
 
-
+#Itemizes the objects in the dictionary and prints them all.
 def analyze_scores(scorebook):
+    for student in scorebook.items():
+        key = student[0]
+        score = student[1]
+        print(f"{key}: {score}")
+
+    #Performs all the changes and prints the average after the changes.
+    scorebook['Dana'] = 85
+    print(f"Updated Dana's score to {scorebook['Dana']}.")
+
+    scorebook.pop('Bob')
+    print('Removed Bob from the scorebook.')
+
+    print(scorebook.get('Eve', 'Eve not found in scorebook.'))
+
+    print(f"Average score: {sum(scorebook.values())/len(scorebook):.2f}")
 
 
-
-
-
+#Creates empty dictionary, then manually adds the values.
 def main():
 
-    scorebook = {
-        'mike' : 60,
-        'ben' : 89,
-        'matthew' : 100,
-        'josh' : 90
-    }
+    scorebook = {}
+
+    scorebook['Alice'] = 88
+    scorebook['Bob'] = 75
+    scorebook['Charlie'] = 93
+    scorebook['Dana'] = 80
 
 
+    analyze_scores(scorebook)
 
-
-
-
+#Initalizes program
 if __name__ == '__main__':
     main()
