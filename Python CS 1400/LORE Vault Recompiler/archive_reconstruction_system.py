a4f2a079b02170d054974e9b0cea041681aa6c0a
Matthew Blair <matthewdblair04@gmail.com>
2025-11-13 19:32:07 -0700

    updated and tried with other text

--- Python CS 1400/LORE Vault Recompiler/archive_reconstruction_system.py
+++ Python CS 1400/LORE Vault Recompiler/archive_reconstruction_system.py
@@ -7,16 +7,19 @@ def text_parser(Bookcode):
     with open(sys.argv[1], "r") as file:
         lines_sortable =[]
         longest_line = "starter"
+        longest_line_index = 0
 
         #splits the lines by the | then adds it to the list as a tuple
         for line in file:
             line_contents = line.split("|")
-            lines_sortable.append((line_contents[1], line_contents[0]))
+            lines_sortable.append((int(line_contents[1]), line_contents[0]))
 
-            #sets the varialbe to the longest line in the file by len it also chooses the highest line number
+
+            #sets the variable to the longest line in the file by len it also chooses the highest line number
             #by nature of going though the iterations of the loop.
-            if len(lines_sortable[0]) > len(longest_line[0]):
-                longest_line = lines_sortable[0]
+            if len(line_contents[0]) > len(longest_line):
+                longest_line = line_contents[0]
+                longest_line_index = line_contents[1]
 
         #sorts the tuples
         sorted_lines = sorted(lines_sortable)
@@ -26,21 +29,22 @@ def text_parser(Bookcode):
 
         average_line = (sum(len(text) for text in lines_second)/len(lines_second))
 
-        external_file(Bookcode, longest_line, average_line, lines_second)
+        external_file(Bookcode, longest_line, longest_line_index, average_line, lines_second)
 
 
 
 
 
+## Both my round and my :.f are rounding, but it seems the rubric is truncating...
 
-def external_file(Bookcode,longest_line, average_line, lines_second):
+def external_file(Bookcode, longest_line, longest_line_index, average_line, lines_second):
     #checks that a file exicts. creates it if it doesn't and writes to it if it does
     try:
         with open(f"{Bookcode}_book.txt", "x") as file:
             print("File not found, creating new file")
             file.write(f"""{Bookcode}\n
-Longest line ({longest_line[0]}): {longest_line[1]}\n
-Average length: {average_line:.3f}\n
+Longest line ({longest_line_index}): {longest_line}\n
+Average length: {round(average_line)}\n
 """)
             for line in lines_second:
                 file.write(f"{line}\n")
@@ -49,8 +53,8 @@ Average length: {average_line:.3f}\n
         print("File Found!")
         with open(f"{Bookcode}_book.txt", "w") as file:
             file.write(f"""{Bookcode}\n
-Longest line ({longest_line[0]}): {longest_line[1]} \n
-Average length: {average_line:.3f}\n
+Longest line ({longest_line_index}): {longest_line} \n
+Average length: {average_line:.0f}\n
 """)
             for line in lines_second:
                 file.write(f"{line}\n")
