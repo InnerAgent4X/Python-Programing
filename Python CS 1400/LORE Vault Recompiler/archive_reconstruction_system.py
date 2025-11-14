570d5b6eaacbb07025ee68b8c065d5fcbc227e58
Matthew Blair <matthewdblair04@gmail.com>
2025-11-13 18:38:14 -0700

    updated the longest line stuff

--- Python CS 1400/LORE Vault Recompiler/archive_reconstruction_system.py
+++ Python CS 1400/LORE Vault Recompiler/archive_reconstruction_system.py
@@ -1,17 +1,22 @@
 import sys
-import codecs
 
 
 #opens the text file, then sorts and exports every line as a tuple with the number first
 def text_parser(Bookcode):
-    #Ignores the unicode in the file so it doesn't break
-    with codecs.open(sys.argv[1], encoding='utf-8') as file:
+
+    with open(sys.argv[1], "r") as file:
         lines_sortable =[]
+        longest_line = "starter"
 
         #splits the lines by the | then adds it to the list as a tuple
         for line in file:
             line_contents = line.split("|")
             lines_sortable.append((line_contents[1], line_contents[0]))
+
+            #sets the varialbe to the longest line in the file by len it also chooses the highest line number
+            #by nature of going though the iterations of the loop.
+            if len(lines_sortable[0]) > len(longest_line[0]):
+                longest_line = lines_sortable[0]
 
         #sorts the tuples
         sorted_lines = sorted(lines_sortable)
@@ -19,28 +24,22 @@ def text_parser(Bookcode):
         #creates a second list of only the text files in order to check for length
         lines_second = [text[1] for text in sorted_lines]
 
-        longest_line = max(lines_second, key=len)
-        longest_line_index = lines_second.index(longest_line)
-        print(f"Longest line ({longest_line_index + 1}): {longest_line}")
-
         average_line = (sum(len(text) for text in lines_second)/len(lines_second))
-        print(f"Average length: {average_line:.3f}")
 
-
-        external_file(Bookcode, longest_line_index, longest_line, average_line, lines_second)
+        external_file(Bookcode, longest_line, average_line, lines_second)
 
 
 
 
 
 
-def external_file(Bookcode, longest_line_index, longest_line, average_line, lines_second):
+def external_file(Bookcode,longest_line, average_line, lines_second):
     #checks that a file exicts. creates it if it doesn't and writes to it if it does
     try:
         with open(f"{Bookcode}_book.txt", "x") as file:
             print("File not found, creating new file")
             file.write(f"""{Bookcode}\n
-Longest line ({longest_line_index}): {longest_line}\n
+Longest line ({longest_line[0]}): {longest_line[1]}\n
 Average length: {average_line:.3f}\n
 """)
             for line in lines_second:
@@ -50,7 +49,7 @@ Average length: {average_line:.3f}\n
         print("File Found!")
         with open(f"{Bookcode}_book.txt", "w") as file:
             file.write(f"""{Bookcode}\n
-Longest line ({longest_line_index}): {longest_line}\n
+Longest line ({longest_line[0]}): {longest_line[1]} \n
 Average length: {average_line:.3f}\n
 """)
             for line in lines_second:
@@ -61,7 +60,6 @@ Average length: {average_line:.3f}\n
 #Displays Bookcode, then calls the text parser function
 def main():
     Bookcode = (f"{sys.argv[1][0]}{sys.argv[1][1]}{sys.argv[1][2]}")
-    print(Bookcode)
 
     text_parser(Bookcode)
 
