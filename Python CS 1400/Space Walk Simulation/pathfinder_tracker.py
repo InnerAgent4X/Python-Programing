08c39b0abfbde332dd8f1d44187d9a0723296b78
Matthew Blair <matthewdblair04@gmail.com>
2025-11-19 18:32:18 -0700

    started space walk

---
+++ Python CS 1400/Space Walk Simulation/pathfinder_tracker.py
@@ -0,0 +1,24 @@
+import sys
+import matplotlib.pyplot as plt
+
+
+
+def graphing(x,y):
+    plt.plot(x,y)
+    plt.show()
+
+
+
+
+
+def main():
+    print("here we go again")
+
+    x = [1,2,3,4,5]
+    y = [1,2,3,4,5]
+    graphing(x,y)
+
+
+
+if __name__ == '__main__':
+    main()
