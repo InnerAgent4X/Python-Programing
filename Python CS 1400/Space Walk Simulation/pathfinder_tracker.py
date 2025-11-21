e478cdc902beaa54050833be65a811aba5370a13
Matthew Blair <matthewdblair04@gmail.com>
2025-11-20 17:19:03 -0700

    Update pathfinder_tracker.py

--- Python CS 1400/Space Walk Simulation/pathfinder_tracker.py
+++ Python CS 1400/Space Walk Simulation/pathfinder_tracker.py
@@ -1,4 +1,3 @@
-import sys
 import matplotlib.pyplot as plt
 import random
 
@@ -20,7 +19,36 @@ def lance_walk(astronauts):
         astronauts["Lance"].append((x, y))
 
 
+def sophie_walk(astronauts):
+    x = 0
+    y = 0
+    for i in range(1, 11):
+        movement = random.choices(["up", "down", "left", "right"], weights = [50, 16.67, 16.67, 16.67], k=1)[0]
+        if movement == "up":
+            y += 1
+        elif movement == "down":
+            y -= 1
+        elif movement == "left":
+            x -= 1
+        elif movement == "right":
+            x += 1
+        else:
+            print("something went wrong")
+        astronauts["Sophie"].append((x, y))
 
+
+def finn_walk(astronauts):
+    x = 0
+    y = 0
+    for i in range(1, 11):
+        movement = random.choice(["left", "right"])
+        if movement == "left":
+            x -= 1
+        elif movement == "right":
+            x += 1
+        else:
+            print("something went wrong")
+        astronauts["Finn"].append((x, y))
 
 
 
@@ -31,7 +59,9 @@ def lance_walk(astronauts):
 def plot_graph(coordinates, color, marker):
 
     for item in coordinates:
-        plt.scatter(item[0], item[1], marker=marker, color=color)
+        x_values = item[0]
+        y_values = item[1]
+        plt.scatter(x_values, y_values, marker=marker, color=color)
 
     #plt.scatter(x_values, y_values, color = color, marker = marker )
     plt.xlabel("x")
@@ -54,6 +84,17 @@ def main():
     coordinates = [astronauts["Lance"]]
     plot_graph(coordinates, "blue", "o")
     print(astronauts["Lance"])
+
+    sophie_walk(astronauts)
+    coordinates = [astronauts["Sophie"]]
+    plot_graph(coordinates, "red", "s")
+    print(astronauts["Sophie"])
+
+    finn_walk(astronauts)
+    coordinates = [astronauts["Finn"]]
+    plot_graph(coordinates, "blue", "o")
+    print(astronauts["Finn"])
+
 
 if __name__ == '__main__':
     main()
