48dd03617a40c0d97f03018e6ae08c79226506e9
Matthew Blair <matthewdblair04@gmail.com>
2025-12-08 17:38:59 -0700

    Update fact_check.py
    got the sorting and date conversion working.

--- Python CS 1400/Programing Final/fact_check.py
+++ Python CS 1400/Programing Final/fact_check.py
@@ -4,6 +4,7 @@ from dateutil.relativedelta import relativedelta
 
 
 def job_data():
+    print("job_data")
     # opens and reads the csv file into a dictionary
     with open('BLS_private_mine.csv', newline='') as csvfile:
         values = csv.DictReader(csvfile)
@@ -21,18 +22,41 @@ def job_data():
 
 #opens presidents doc and creates a list
 def presidents_parser():
+    print("presidents_parser")
     pres_data = []
+    democrats = []
+    republicans = []
     with open('presidents.txt', 'r') as file:
         for line in file:
             #creates a list and strips the spaces and splits on pipes
             pres_data.append([x.strip() for x in line.strip().split('|')])
 
+        #converts the string date data into dates
+        for item in pres_data:
+            item[1] = date(year= int(item[2]), month= int(item[1]), day= 1)
+            item[3] = date(year= int(item[4]), month= int(item[3]), day=1)
+            item.remove(item[2])
+            item.remove(item[3])
+
+
+        #sorts items into lists
         for row in pres_data:
-            pres_data[1] = date(pres_data[2],pres_data[1],1 )
-            pres_data[3] = date(pres_data[4],pres_data[3],1 )
+            if row[-1] == 'Democratic':
+                democrats.append(row)
+            elif row[-1] == 'Republican':
+                republicans.append(row)
+            else:
+                print('Error')
+        print(democrats)
+        print(republicans)
 
-    print(pres_data)
 
+
+        for pres in pres_data:
+            term = relativedelta(pres[2], pres[1])
+            diff_months = term.years * 12 + term.months
+
+            print(diff_months)
 
 
 
@@ -40,10 +64,9 @@ def presidents_parser():
 
 
 def main():
-    print("test")
+    print("main")
     presidents_parser()
     #job_data()
-
 
 
 
